79c78b62dc5dd51f5404109e24905d925739038a
dimonov <evildestroyer@gmail.com>
2010-10-14 20:28:31 -0400

    license change

--- src/dialog.c
+++ src/dialog.c
@@ -1,18 +1,29 @@
-
 /*
- * TMSNC - Textbased MSN Client Copyright (C) 2004 The IR Developer Group
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the IR Public Domain License as published by the IR Group;
- * either version 1.6 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the IR Public Domain License along with
- * this program; if not, write to sanoix@gmail.com.
- */
+This file is part of MSNC.
+
+Copyright (C) 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
+2003, 2004, 2005  Free Software Foundation, Inc.
+This Makefile.in is free software; the Free Software Foundation
+gives unlimited permission to copy and/or distribute it,
+with or without modifications, as long as this notice is preserved.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY, to the extent permitted by law; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+MSNC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MSNC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "common.h"
 #include "dialog.h"
